a5592312db269f1fd47bc5b5bdf2e28dd05099e6
Jeasmine Ñahui <jeasmine.nahui@moove-it.com>
2017-08-29 11:23:35 -0300

    Improve library code

---
+++ step-control-ios/Constants.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+struct Constants {
+  struct Colors {
+    static let foregroundCGColor = UIColor.white.cgColor
+    static let unselectedCGColor = UIColor.gray.cgColor
+    static let selectedCGColor = UIColor.orange.cgColor
+    static let completedCGColor = UIColor.orange.cgColor
+    static let selectedColor = UIColor.orange
+    static let unselectedColor = UIColor.gray
+  }
+}
--- step-control-ios/DotTextLayer.swift
+++ step-control-ios/DotTextLayer.swift
@@ -1,70 +1,63 @@
-//
-//  DotTextLayer.swift
-//  StepsPager
-//
-//  Created by Juan Pereira on 6/7/17.
-//  Copyright © 2017 Juan Pereira. All rights reserved.
-//
-// with help of https://stackoverflow.com/questions/4765461/vertically-align-text-in-a-catextlayer
-
 import Foundation
 import UIKit
 import QuartzCore
 
+//Help from: https://stackoverflow.com/questions/4765461/vertically-align-text-in-a-catextlayer
 class DotTextLayer: CATextLayer {
-
-    let defaultFontSize: CGFloat = 14
-    let defaultForegroundColor = UIColor.white.cgColor
-    let defaultAlignmentMode = "center"
-
-    var isSelected = false
-    var isCompleted = false
-    var unselectedColor = UIColor.gray.cgColor
-    var selectedColor = UIColor.orange.cgColor
-    var completedColor = UIColor.orange.cgColor
-    var index = 0
-
-    override var frame: CGRect {
-        didSet {
-            setDefaultValues()
-            setNeedsDisplay()
-        }
+  
+  let defaultForegroundColor = Constants.Colors.foregroundCGColor
+  let defaultFontSize: CGFloat = 14
+  let defaultAlignmentMode = "center"
+  
+  var unselectedColor = Constants.Colors.unselectedCGColor
+  var selectedColor = Constants.Colors.selectedCGColor
+  var completedColor = Constants.Colors.completedCGColor
+  
+  var isSelected = false
+  var isCompleted = false
+  var index = 0
+  
+  override var frame: CGRect {
+    didSet {
+      setDefaultValues()
+      setNeedsDisplay()
     }
-
-    override open func draw(in ctx: CGContext) {
-        let yDiff: CGFloat
-        let fontSize: CGFloat
-        let height = self.bounds.height
-
-        if let attributedString = self.string as? NSAttributedString {
-            fontSize = attributedString.size().height
-            yDiff = (height-fontSize)/2
-        } else {
-            fontSize = self.fontSize
-            yDiff = (height-fontSize)/2 - fontSize/10
-        }
-
-        ctx.saveGState()
-        ctx.translateBy(x: 0.0, y: yDiff)
-        super.draw(in: ctx)
-        ctx.restoreGState()
+  }
+  
+  override open func draw(in ctx: CGContext) {
+    let yDiff: CGFloat
+    let fontSize: CGFloat
+    let height = bounds.height
+    
+    if let attributedString = string as? NSAttributedString {
+      fontSize = attributedString.size().height
+      yDiff = (height-fontSize)/2
+    } else {
+      fontSize = self.fontSize
+      yDiff = (height-fontSize)/2 - fontSize/10
     }
-
-    fileprivate func setDefaultValues() {
-        if (isCompleted) {
-            string = "✓"
-            backgroundColor = completedColor
-        } else if (isSelected) {
-            string = String(index)
-            backgroundColor = selectedColor
-        } else {
-            string = nil
-            backgroundColor = unselectedColor
-        }
-
-        foregroundColor = defaultForegroundColor
-        fontSize = defaultFontSize
-        alignmentMode = defaultAlignmentMode
-        cornerRadius = self.frame.width / 2.0
+    
+    ctx.saveGState()
+    ctx.translateBy(x: 0.0, y: yDiff)
+    super.draw(in: ctx)
+    ctx.restoreGState()
+  }
+  
+  fileprivate func setDefaultValues() {
+    if (isCompleted) {
+      string = "✓"
+      backgroundColor = completedColor
+    } else if (isSelected) {
+      string = String(index)
+      backgroundColor = selectedColor
+    } else {
+      string = nil
+      backgroundColor = unselectedColor
     }
+    
+    foregroundColor = defaultForegroundColor
+    fontSize = defaultFontSize
+    alignmentMode = defaultAlignmentMode
+    cornerRadius = frame.width / 2.0
+  }
 }
